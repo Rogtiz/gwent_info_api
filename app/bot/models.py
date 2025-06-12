815d9060fb16eefb35ab95c5a125323f3b787cff
Eduard Rayevskiy <75925110+Rogtiz@users.noreply.github.com>
2025-06-12 06:57:38 +0000

    create bot models

---
+++ app/bot/models.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from sqlalchemy import BigInteger, Boolean, Column, DateTime, Integer, String
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+
+class Properties(Base):
+    __tablename__ = "properties"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+    value = Column(String, nullable=False)
+    description = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, onupdate=datetime.utcnow)
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    chat_id = Column(String, unique=True)
+    full_name = Column(String)
+    description = Column(String)
+    disabled = Column(Boolean, default=False)
+    admin_level = Column(Integer, default=0)
+    is_banned = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, onupdate=datetime.utcnow)
+
+
+class Groups(Base):
+    __tablename__ = "groups"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    chat_id = Column(String, unique=True)
+    description = Column(String)
+    disabled = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, onupdate=datetime.utcnow)
+
+
+class Feedbacks(Base):
+    __tablename__ = "feedbacks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, nullable=False)
+    chat_id = Column(String, nullable=False)
+    message = Column(String, nullable=False)
+    is_fixed = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, onupdate=datetime.utcnow)
