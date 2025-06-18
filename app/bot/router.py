71ed0d8171d2704cc4029dfb4dace6aaa2c35e2b
Eduard Rayevskiy <75925110+Rogtiz@users.noreply.github.com>
2025-06-18 11:26:20 +0000

    fixed bugs in property router

--- app/bot/router.py
+++ app/bot/router.py
@@ -61,7 +61,7 @@ async def get_property(key: str) -> PropertySchema:
         return cached_value
     property_value = await PropertiesDAO.find_one_or_none(name=key)
     if property_value:
-        await redis_client.set(key, PropertySchema.from_orm(property_value).model_dump(), ex=3600)
+        await redis_client.set(key, PropertySchema.from_orm(property_value).model_dump_json(), ex=3600)
         return property_value
     raise HTTPException(status_code=404, detail="Property not found")
 
