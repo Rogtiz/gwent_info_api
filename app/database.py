48bbaaf3e35f1a115b0e69c5b4527249be70b554
Eduard Rayevskiy <75925110+Rogtiz@users.noreply.github.com>
2025-06-19 14:56:12 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from app.config import DATABASE_URL
 
 # DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_async_engine(DATABASE_URL) # echo=True
+engine = create_async_engine(DATABASE_URL, echo=False, pool_pre_ping=True) # echo=True
 
 async_session_maker = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
